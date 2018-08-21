1f888b3cb5ee6c67be368382cba58c313a6597bf
aceroAD <diegoacero.inef@gmail.com>
2018-08-21 23:04:51 +0200

    añadir funciones

--- EditorTextoSimple/src/graficos/Menu.java
+++ EditorTextoSimple/src/graficos/Menu.java
@@ -14,19 +14,32 @@ public class Menu extends JMenuBar{
 		this.add(archivo);
 		this.add(estilos);
 		
+		//Menu Items for archivo
+		JMenuItem open = new JMenuItem("Open");
+		JMenuItem newDoc = new JMenuItem("New Document");
+		JMenuItem save =new JMenuItem("Save");
+		JMenuItem saveAs =new JMenuItem("Save as");
+		JMenuItem exit =new JMenuItem("Exit");
+		
+		//Menu Items for estilos
+		JMenuItem font = new JMenuItem("Font");
+		JMenuItem fontSize =new JMenuItem("Font Size");
+		
 		//contruct Archivo menu
-		this.archivo.add(new JMenuItem("New Document"));
-		this.archivo.add(new JSeparator());
-		this.archivo.add(new JMenuItem("Save"));
-		this.archivo.add(new JSeparator());
-		this.archivo.add(new JMenuItem("Save as"));
+		this.archivo.add(open);
 		this.archivo.addSeparator();
-		this.archivo.add(new JMenuItem("Exit"));
+		this.archivo.add(newDoc);
+		this.archivo.add(new JSeparator());
+		this.archivo.add(save);
+		this.archivo.add(new JSeparator());
+		this.archivo.add(saveAs);
+		this.archivo.addSeparator();
+		this.archivo.add(exit);
 		
 		//contruct style menu
-		this.estilos.add(new JMenuItem("Font"));
+		this.estilos.add(font);
 		this.estilos.addSeparator();
-		this.estilos.add(new JMenuItem("Size"));
+		this.estilos.add(fontSize);
 		
 	}
 
